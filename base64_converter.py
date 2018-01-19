84cbf3bedb51d18810186f2f0016dac3f6d75b74
HL <havardlangdal@gmail.com>
2018-01-19 15:48:29 +0100

    base64 converter. If we need to mine our database for image data, or if tensorflow only accepts certain formats, this could be helpful. Succesfully managed to convert data from websocket to format as b64 and reassemble as image on our server.

---
+++ base64_converter.py
@@ -0,0 +1,13 @@
+import base64 as b64
+import re
+# Inspo https://stackoverflow.com/questions/41957490/send-canvas-image-data-uint8clampedarray-to-flask-server-via-ajax
+
+
+# this method converts a base 64 string to an image
+# img_string is an string representation of an image
+def convertToImg(img_string):
+    img_data = b64.standard_b64decode(re.sub('^data:image/.+;base64,', '', img_string))
+    file = 'testfile.png'
+    # print("Image data: ", img_data)
+    with open(file, 'wb') as fh: # filehandler
+        fh.write(img_data)
