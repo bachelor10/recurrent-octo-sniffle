51b6806c370eddf204258e7379c56890db92f060
torkilso <torkil.solheim@gmail.com>
2018-01-22 16:41:45 +0100

    Added tinydb for soutions and timestamp to images

--- base64_converter.py
+++ base64_converter.py
@@ -1,5 +1,7 @@
 import base64 as b64
-import re
+import re, time, json
+from tinydb import TinyDB, Query
+
 # Inspo https://stackoverflow.com/questions/41957490/send-canvas-image-data-uint8clampedarray-to-flask-server-via-ajax
 
 
@@ -7,7 +9,15 @@ import re
 # img_string is an string representation of an image
 def convertToImg(img_string, equation):
     img_data = b64.standard_b64decode(re.sub('^data:image/.+;base64,', '', img_string))
-    file = './bitmap_data/' + equation + '.png'
-    #print("Image data: ", img_data)
+    
+    ms = str(int(round(time.time() * 1000)))
+    
+    file = './bitmap_data/' + ms + '.png'
+
     with open(file, 'wb') as fh: # filehandler
         fh.write(img_data)
+
+    with TinyDB('solutions.json') as db:
+        db.insert({ms: equation})
+
+    
