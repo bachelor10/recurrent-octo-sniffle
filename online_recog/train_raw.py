622dc990ce7c67856cc90598e319b3e9416daba3
HL <havardlangdal@gmail.com>
2018-02-21 15:47:11 +0100

    Almost ready to work on RNN model

---
+++ online_recog/train_raw.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Embedding
+from keras.layers import GRU
+from keras.datasets import boston_housing
+
+max_features = 20000
+maxlen = 80  # cut texts after this number of words (among top max_features most common words)
+batch_size = 32
+
+print('Loading data...')
+(x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
+
+print('Pad sequences (samples x time)')
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+
+print('Build model...')
+model = Sequential()
+model.add(Embedding(max_features, 128))
+model.add(GRU(128, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
+
+# try using different optimizers and different optimizer configs
+#model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+print('Train...')
+#model.fit(x_train, y_train, batch_size=batch_size, epochs=2, validation_data=(x_test, y_test))
+#score, acc = model.evaluate(x_test, y_test, batch_size=batch_size)
+#print('Test score:', score)
+#print('Test accuracy:', acc)
+
+model.save('imdb_test.h5')
---
+++ online_recog/xml_parse_rawdata.py
@@ -0,0 +1,85 @@
+import xml.etree.ElementTree as ET
+import uuid, math, time
+import numpy as np
+from PIL import Image, ImageDraw
+from itertools import cycle
+import random
+import os
+import matplotlib.pyplot as plt
+from rdp import rdp, rdp_iter  # https://pypi.python.org/pypi/rdp
+
+from machine_learning.xml_parse import format_trace, find_segments
+
+class SeqParser:
+	def __init__(self):
+		print("s")
+	
+
+
+def plot_trace(trace): # trace is x,y coordinates
+	plt.plot(trace[:, 0], trace[:, 1])
+	axes = plt.gca()
+	axes.set_xlim([np.min(trace[:, 0]) - 1.0, np.max(trace[:, 0]) + 1.0])
+	axes.set_ylim([np.min(trace[:, 1]) - 1.0, np.max(trace[:, 1]) + 1.0])
+	
+	plt.show()
+
+
+
+def consecutive_segments(segm):
+	print("SEGM", segm)
+	print(type(segm))
+	for i, t in enumerate(segm):
+		if True:
+			print(t['truth'])
+			parse_single_segment(t)
+
+
+def find_tracelength(trace):
+	lengths = []
+	for i, t in enumerate(trace):
+		lengths.append(len(t))
+	return lengths
+
+# this method is meant to parse a segment, sequentially return data and truth
+# https://www.tensorflow.org/versions/master/tutorials/recurrent_quickdraw
+def parse_single_segment(segment):
+	truth = segment['truth']
+	traces = segment['traces']
+	stroke_lengths = find_tracelength(traces)
+	total_points = sum(stroke_lengths)
+	np_ink = np.zeros((total_points, 3), dtype=np.float32)
+	it = 0
+	
+	for trace in traces:
+		trace = np.array(trace).astype(np.float32)
+		np_ink[it:(it + len(trace)), 0:2] = trace
+		it += len(trace)
+		np_ink[it -1, 2] = 1 # stroke end
+		
+	
+	
+	lower_x = np.min(np_ink[:, 0:2], axis=0)
+	lower_y = np.min(np_ink[:, 0:2], axis=0)
+	upper_x = np.max(np_ink[:, 0:2], axis=0)
+	upper_y = np.max(np_ink[:, 0:2], axis=0)
+	
+	plot_trace(np_ink)
+	
+	relation_between = upper_x - lower_x
+	relation_between[relation_between == 0] = 1
+	np_ink[:, 0:2] = (np_ink[:, 0:2] - lower_x) / relation_between
+	#print(np_ink)
+	#plot_trace(np_ink)
+	np_ink = np_ink[1:, 0:2] - np_ink[0:-1, 0:2]
+	#print (np_ink)
+	return np_ink, truth
+	
+
+if __name__ == '__main__':
+	tree = ET.parse('01.inkml')
+	root = tree.getroot()
+	segments = find_segments(root) # gets a list of Segment objects
+
+	# print(segments)
+	consecutive_segments(segments)
