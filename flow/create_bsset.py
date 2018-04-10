131381cf77b5f4608952b7180c4fb96cebbb7fa2
Håv <havardlangdal@gmail.com>
2018-04-10 16:58:53 +0200

    I am now able to create ndjson files, should eventually give us something to write about on the report.

--- flow/create_bsset.py
+++ flow/create_bsset.py
@@ -30,7 +30,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import xml.etree.ElementTree as ET
 import argparse
 import json
 import os
@@ -39,38 +38,41 @@ import sys
 import numpy as np
 import tensorflow as tf
 
-from online_recog.create_dataset import find_tracelength
 
-
-def parse_line(segment):
-	truth = segment.truth
-	traces = segment.traces
-	stroke_lengths = find_tracelength(traces)
+def parse_line(ndjson_line):
+	"""Parse an ndjson line and return ink (as np array) and classname."""
+	sample = json.loads(ndjson_line)
+	class_name = sample["class"]
+	if not class_name:
+		print ("Empty classname")
+		return None, None
+	inkarray = sample["drawing"]
+	stroke_lengths = [len(stroke[0]) for stroke in inkarray]
 	total_points = sum(stroke_lengths)
 	np_ink = np.zeros((total_points, 3), dtype=np.float32)
-	it = 0
-	if not traces:
-		print("No trace provided.")
+	current_t = 0
+	if not inkarray:
+		print("Empty inkarray")
 		return None, None
-	
-	for i, trace in enumerate(traces):
-		trace = np.array(trace).astype(np.float32)
-		np_ink[it:(it + len(trace)), 0:2] = trace
-		it += len(trace)
-		np_ink[it - 1, 2] = 1  # stroke end
-	# print("LOWER FAILS: ", np_ink)
+	for stroke in inkarray:
+		if len(stroke[0]) != len(stroke[1]):
+			print("Inconsistent number of x and y coordinates.")
+			return None, None
+		for i in [0, 1]:
+			np_ink[current_t:(current_t + len(stroke[0])), i] = stroke[i]
+		current_t += len(stroke[0])
+		np_ink[current_t - 1, 2] = 1  # stroke_end
+	# Preprocessing.
+	# 1. Size normalization.
 	lower = np.min(np_ink[:, 0:2], axis=0)
 	upper = np.max(np_ink[:, 0:2], axis=0)
-	
-	# plot_trace(np_ink)
-	
-	relation_between = upper - lower
-	relation_between[relation_between == 0] = 1
-	np_ink[:, 0:2] = (np_ink[:, 0:2] - lower) / relation_between
-	
-	np_ink = np_ink[1:, 0:2] - np_ink[0:-1, 0:2]
+	scale = upper - lower
+	scale[scale == 0] = 1
+	np_ink[:, 0:2] = (np_ink[:, 0:2] - lower) / scale
+	# 2. Compute deltas.
+	np_ink[1:, 0:2] -= np_ink[0:-1, 0:2]
 	np_ink = np_ink[1:, :]
-	return np_ink, truth
+	return np_ink, class_name
 
 
 def convert_data(trainingdata_dir,
@@ -80,28 +82,26 @@ def convert_data(trainingdata_dir,
 				 output_shards=10,
 				 offset=0):
 	"""Convert training data from ndjson files into tf.Example in tf.Record.
-  Args:
-   trainingdata_dir: path to the directory containin the training data.
-     The training data is stored in that directory as ndjson files.
-   observations_per_class: the number of items to load per class.
-   output_file: path where to write the output.
-   classnames: array with classnames - is auto created if not passed in.
-   output_shards: the number of shards to write the output in.
-   offset: the number of items to skip at the beginning of each file.
-  Returns:
-    classnames: the class names as strings. classnames[classes[i]] is the
-      textual representation of the class of the i-th data point.
-  """
+	Args:
+	 trainingdata_dir: path to the directory containin the training data.
+	   The training data is stored in that directory as ndjson files.
+	 observations_per_class: the number of items to load per class.
+	 output_file: path where to write the output.
+	 classnames: array with classnames - is auto created if not passed in.
+	 output_shards: the number of shards to write the output in.
+	 offset: the number of items to skip at the beginning of each file.
+	Returns:
+	  classnames: the class names as strings. classnames[classes[i]] is the
+		textual representation of the class of the i-th data point.
+	"""
 	
 	def _pick_output_shard():
 		return random.randint(0, output_shards - 1)
 	
 	file_handles = []
 	# Open all input files.
-	print("Trying to parse %s " % trainingdata_dir)
-	
 	for filename in sorted(tf.gfile.ListDirectory(trainingdata_dir)):
-		if not filename.endswith(".inkml"):
+		if not filename.endswith(".ndjson"):
 			print("Skipping", filename)
 			continue
 		file_handles.append(
@@ -122,39 +122,34 @@ def convert_data(trainingdata_dir,
 	reading_order = list(range(len(file_handles))) * observations_per_class
 	random.shuffle(reading_order)
 	
-	for c in reading_order: # reading order is a list of numbers.
-		#print(file_handles[c])
-		
-		
-		# LOCAL IMPORT !
-		from machine_learning.xml_parse import find_segments
-		from online_recog.create_dataset import get_inkml_root
-		root = get_inkml_root(file_handles[c].name)
-	
-		segm = find_segments(root)
-		
-		for i, t in enumerate(segm):
-			ink, class_name = parse_line(t)
-			
-			if class_name not in classnames:
-				classnames.append(class_name)
+	for c in reading_order:
+		print("Fetching ink from %s ", file_handles[c].name)
+		line = file_handles[c].readline()
+		ink = None
+		while ink is None:
 			try:
-				features = {}
-				features["truth_index"] = tf.train.Feature(int64_list=tf.train.Int64List(
-					value=[classnames.index(class_name)]))
+				ink, class_name = parse_line(line)
+				if ink is None:
+					print ("Couldn't parse ink from '" + line + "'.")
+				if class_name not in classnames:
+					classnames.append(class_name)
+			except json.JSONDecodeError as jde:
+				print("Error parsing from file : ", file_handles[c].name)
+				ink = None
 				
-				features["ink"] = tf.train.Feature(float_list=tf.train.FloatList(
-					value=ink.flatten()))
-				
-				features["shape"] = tf.train.Feature(int64_list=tf.train.Int64List(
-					value=ink.shape))
-			
-				f = tf.train.Features(feature=features)
-				example = tf.train.Example(features=f)
-				writers[_pick_output_shard()].write(example.SerializeToString())
-			except ValueError as ve:
-				print(ve)
-				
+		if ink is not None:
+			features = {}
+			features["class_index"] = tf.train.Feature(int64_list=tf.train.Int64List(
+				value=[classnames.index(class_name)]))
+			features["ink"] = tf.train.Feature(float_list=tf.train.FloatList(
+				value=ink.flatten()))
+			features["shape"] = tf.train.Feature(int64_list=tf.train.Int64List(
+				value=ink.shape))
+			f = tf.train.Features(feature=features)
+			example = tf.train.Example(features=f)
+			writers[_pick_output_shard()].write(example.SerializeToString())
+		else:
+			print("Ink is none. From file ", file_handles[c].name)
 	
 	# Close all files
 	for w in writers:
@@ -162,7 +157,7 @@ def convert_data(trainingdata_dir,
 	for f in file_handles:
 		f.close()
 	# Write the class list.
-	with tf.gfile.GFile(output_file + ".classes", "w") as f: # no need to close, with closes
+	with tf.gfile.GFile(output_file + ".classes", "w") as f:
 		for class_name in classnames:
 			f.write(class_name + "\n")
 	return classnames
@@ -192,22 +187,22 @@ if __name__ == "__main__":
 	parser.add_argument(
 		"--ndjson_path",
 		type=str,
-		default=".",
-		help="Directory where the inkml files are stored.")
+		default="DATA",
+		help="Directory where the ndjson files are stored.")
 	parser.add_argument(
 		"--output_path",
 		type=str,
-		default="DATA",
+		default="records",
 		help="Directory where to store the output TFRecord files.")
 	parser.add_argument(
 		"--train_observations_per_class",
 		type=int,
-		default=2000,
+		default=10000,
 		help="How many items per class to load for training.")
 	parser.add_argument(
 		"--eval_observations_per_class",
 		type=int,
-		default=300,
+		default=1000,
 		help="How many items per class to load for evaluation.")
 	parser.add_argument(
 		"--output_shards",
--- flow/data_assemble.py
+++ flow/data_assemble.py
@@ -1,11 +1,77 @@
 import json
+import codecs
+import os
 import xml.etree.ElementTree as ET
 import numpy as np
 from machine_learning.xml_parse import find_segments
 from online_recog.create_dataset import find_tracelength
+from rdp import rdp
+import newlinejson as nlj
+
+remove_punctuation_map = dict((ord(char), None) for char in '\/*?:"<>|')
+
 
 def get_inkml_root(file):
 	return ET.parse(file).getroot()
+
+
+'''
+writeclass_ndjson
+Writes traces to an ndjson file
+'''
+
+
+def __writeclass_ndjson(trace_data, class_name, out_path = "DATA"):
+	out_data = list()
+	for trace in trace_data:
+		x = trace[:,0]
+		y = trace[:,1]
+		tmp = list()
+		tmp.append(x.tolist())
+		tmp.append(y.tolist())
+		out_data.append(tmp)
+	# creaing map
+	data = {}
+	data['class'] = class_name
+	data['drawing'] = out_data
+	with nlj.open("DATA/" + class_name.strip('\\') + ".ndjson", "a") as dst:
+		dst.write(data)
+	
+
+
+'''
+scale_uniform
+Scales data into a 256x256 region
+Uses RDP to reduce number of strokes
+
+'''
+
+
+def __scale_uniform(data, class_name, max=255):
+	if len(data) > 1:
+		for t in data:
+			low = np.min(t[:])
+			upp = np.max(t[:])
+	else:
+		low = np.min(data[:])
+		upp = np.max(data[:])
+	# print("Low: ", low, "\nUpper: ", upp)
+	scale = max/upp
+	
+	if len(data) > 1:
+		data = np.asarray(data[:]) * scale
+		for i, d in enumerate(data):
+			data[i] = d.astype(int)
+	else:
+		data = np.asarray(data) * scale
+		data = data.astype(int)
+
+	output_data = []
+	for trace in data:
+		newtrace = rdp(trace, epsilon=1.0)
+		output_data.append(newtrace)
+	__writeclass_ndjson(output_data, class_name)
+
 
 def convert_segment_tojson(segment):  # segment object
 	truth = segment.truth
@@ -25,11 +91,36 @@ def convert_segment_tojson(segment):  # segment object
 		it += len(trace)
 		np_ink[it - 1, 2] = 1  # stroke end
 	return traces, truth
-		
+	lower = np.min(np_ink[:, 0:2], axis=0)
+	upper = np.max(np_ink[:, 0:2], axis=0)
+	
+	# plot_trace(np_ink)
+	
+	relation_between = upper - lower
+	relation_between[relation_between == 0] = 1
+	np_ink[:, 0:2] = (np_ink[:, 0:2] - lower) / relation_between
+	
+	np_ink = np_ink[1:, 0:2] - np_ink[0:-1, 0:2]
+	np_ink = np_ink[1:, :]
+
+
 if __name__ == '__main__':
-	root = get_inkml_root('01.inkml')
-	segments = find_segments(root)  # gets a list of Segment objects
-	for s in segments:
-		trace, trt = convert_segment_tojson(s)
-		print("TRACE: ", trace)
-		print("TRUTH: ", trt)
+	data_dir = os.path.dirname(os.path.abspath(__file__)) + "/../online_recog/GT"
+	
+	for file in os.listdir(data_dir):
+		if file.endswith(".inkml"):
+			root = get_inkml_root(os.path.join(data_dir, file))
+			segments = find_segments(root)  # gets a list of Segment objects
+			for s in segments:
+				traces = s.traces
+				truth = s.truth
+				if traces is not None:
+					if len(traces) > 0:
+						if len(traces[0]) > 1:
+							__scale_uniform(traces, truth)
+						else:
+							print("Skipping trace from file %s, because of trace size. " % file)
+					else:
+						print("Skipping trace from file %s, because of missing trace. " % file)
+		else:
+			print("Skipping: ", file)
